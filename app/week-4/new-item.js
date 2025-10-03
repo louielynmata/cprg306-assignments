8c2e60adc4d3ff55680f326cde1c96e1318b42f3
Louielyn Mata <louielynmata@gmail.com>
2025-10-03 02:58:49 -0600

    Added app\week-4 folders with 2 new pages - new-item.js  and page.js, Added TO DO comment on page.js

---
+++ app/week-4/new-item.js
@@ -0,0 +1,7 @@
+// TO DO:
+// Edit page.js to be a Next.js page component that renders the NewItem component.
+
+// TO DO:
+// Setup NewItem Component
+// Add the "use client" directive to the top of the file.
+// Import the useState hook from React.
