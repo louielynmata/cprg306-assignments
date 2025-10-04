7b0ff766a3856b29c582dd6b88050d4013ffe6e2
Louielyn Mata <louielynmata@gmail.com>
2025-10-03 22:02:02 -0600

    Added "use client" and useState import.  Made NewItem component and added the use state.

---
+++ app/week-4/page.js
@@ -0,0 +1,22 @@
+"use client";
+import useState from 'react';
+
+export default function NewItem() {
+  // Use states
+  const [quantity, setQuantity] = useState(1);
+  
+  
+  // TO DO: Make increment and decrement function
+  function increment() {
+  }
+
+  function decrement() {
+
+  }
+
+  // TO DO: Set-up render
+  // TO DO: Style Component
+  return(
+
+  );
+}
