2b4f12375efe4c7cc108e1a855f65fb164a092bb
Louielyn Mata <louielynmata@gmail.com>
2025-10-03 22:41:57 -0600

    Page.js - Implemented increment and decrement function  with conditionals. Added return render jsx with h1 and buttons, Styles.js - Added import and added container and button general styling, Rearranged and added comments as well.

--- app/styles.js
+++ app/styles.js
@@ -1,8 +1,12 @@
 // Global Tailwind CSS styling constants, they are stored in this file for reusability.
 
+// Text Styling
 export const linkStyling =
   "text-blue-300 font-semibold hover:underline hover:text-pink-200 p-2 inline-flex";
 
+export const h1Styling = "text-2xl font-bold mb-3";
+
+// Container Styling
 export const pageContainer =
   "flex min-h-screen flex-col items-center justify-center p-24 w-3xl";
 
@@ -11,4 +15,7 @@ export const pageContainerMed =
 
 export const navContainer = "flex-auto flex-col gap-2 mt-5";
 
-export const h1Styling = "text-2xl font-bold mb-3";
+export const whiteContainer = "bg-blue-100 p-6 rounded-lg";
+
+export const Button =
+  "py-3 px-6 text-2xl rounded-md transition-colors transition-duration-500";
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,22 +1,56 @@
+/*
+Louielyn Mata - CRPG 306-B WEBDEV2
+Assignment no. 3
+*/
 "use client";
-import useState from 'react';
+import { useState } from "react";
+import { pageContainerMed, h1Styling, whiteContainer, Button } from "../styles";
+import NewItem from "./new-item";
 
-export default function NewItem() {
+export default function Page() {
   // Use states
   const [quantity, setQuantity] = useState(1);
-  
-  
+
   // TO DO: Make increment and decrement function
   function increment() {
+    setQuantity(quantity < 20 ? quantity + 1 : quantity);
   }
 
   function decrement() {
-
+    setQuantity(quantity > 1 ? quantity - 1 : quantity);
   }
 
   // TO DO: Set-up render
   // TO DO: Style Component
-  return(
+  return (
+    <main className={pageContainerMed}>
+      <header>
+        <h1 className={`${h1Styling} text-left w-full`}>Add New Item</h1>
+      </header>
 
+      <section className={whiteContainer}>
+        <h2 className="text-xl">
+          <span className="text-gray-700">Quantity: </span>
+          <span className="font-bold text-blue-800">{quantity}</span>
+        </h2>
+
+        <div className="flex flex-row gap-4 my-4 w-full justify-items-start">
+          <button
+            onClick={decrement}
+            className={`${Button} text-gray-900 bg-gray-300 hover:bg-gray-400`}
+          >
+            -
+          </button>
+
+          <button
+            onClick={increment}
+            className={`${Button} bg-blue-700  hover:bg-blue-500`}
+          >
+            +
+          </button>
+        </div>
+        <p className="text-gray-500 text-sm">Allowed Range: 1-20</p>
+      </section>
+    </main>
   );
 }
